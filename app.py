ba0d42bd037a8f83fabad50b8475d635d3b58c26
peiyi-c <pennieyoyoyo@gmail.com>
2024-05-28 23:03:56 +0200

    edit metrics

--- app.py
+++ app.py
@@ -9,30 +9,28 @@ from dateutil.relativedelta import relativedelta
 st.set_page_config(page_title="Stock Dashboard", page_icon="🧊", layout="centered")
 
 # custom styles
-st.markdown('<style>h3{font-size:1.65rem;opacity:0.3;}div.block-container{padding:1rem auto;} div.element-container{margin: 0rem; padding: 0rem;}div[role="tablist"]{justify-content: space-between;}button[role="tab"]{padding: 1rem; border-radius: 10px;}button[role="tab"][aria-selected="true"]{background: #E0FBE2;}button[role="tab"] p, summary{color:#333333; font-weight:600;} summary:hover span{color:#ACE1AF; font-weight:bold;} div[role="presentation"]{background:none;} div[role="alert"]{background:#F6F5F250;color:#5AB2FF;} a{color:rgb(49, 51, 63) !important; text-decoration:none;} a:hover{color:#ACE1AF !important; text-decoration:none;} div[data-baseweb="select"]>div{border-color: #D2E9E9 !important; box-shadow: rgba(0, 0, 0, 0.04) 0px 3px 5px;}div[data-testid="stTable"]{padding: 7px 0 0 0;}table{font-family:"Source Sans Pro", sans-serif;text-align:left;border-radius: 10px;}.no-data{padding: 1.75rem 0;text-align: left; color: rgb(49, 51, 63); font-weight: bold;} .tab-num{font-size: 2.25rem; color: rgb(49, 51, 63);}</style>', unsafe_allow_html = True)
+st.markdown('<style>h3{font-size:1.65rem;opacity:0.3;}div.block-container{padding:1rem auto;} div.element-container{margin: 0rem; padding: 0rem;}div[role="tablist"]{justify-content: space-between;}button[role="tab"]{padding: 1rem; border-radius: 10px;}button[role="tab"][aria-selected="true"]{background: #E0FBE2;}button[role="tab"] p, summary{color:#333333; font-weight:600;} summary:hover span{color:#ACE1AF; font-weight:bold;} div[role="presentation"]{background:none;} div[role="alert"]{padding:1.2rem 2.2rem;background:#F6F5F250;color:#333333;line-height: 1.7;} a{color:rgb(49, 51, 63) !important; text-decoration:none;} a:hover{color:#ACE1AF !important; text-decoration:none;} div[data-baseweb="select"]>div{border-color: #D2E9E9 !important; box-shadow: rgba(0, 0, 0, 0.04) 0px 3px 5px;}div[data-testid="stTable"]{padding: 7px 0 0 0;}table{font-family:"Source Sans Pro", sans-serif;text-align:left;border-radius: 10px;}.no-data{padding: 1.75rem 0;text-align: left; color: rgb(49, 51, 63); font-weight: bold;} .tab-num{font-size: 2.25rem; color: rgb(49, 51, 63);}</style>', unsafe_allow_html = True)
 
 # Title #   
 st.subheader('Stock Dashboard')  
 
 col1, col2 = st.columns((2))
 # Selectbox: Ticker #
-def get_trend(company):
-  if date.today().weekday() == 0 or date.today().weekday() == 5 or date.today().weekday() == 6:
-    return ''
-  elif round(company.info['currentPrice'] - company.basic_info['lastPrice'], 3) > 0:
-    return round(company.info['currentPrice'] - company.basic_info['lastPrice'], 3)
-  else:
-    return ''
 
+  
 with col1:
   ticker_name_list = pd.read_csv('./ticker_names.txt')
   ticker = st.selectbox('Select a Stock', ticker_name_list) 
-  # Retrieve Data
+  # ============= Retrieve Data ==============
   company = yf.Ticker(ticker) 
+  # ============== Company Data ==============
   infos = company.info
+  basic_info = company.basic_info
+  financials = company.financials
+  cashflow = company.cashflow
+   # =========================================
 # Current Price #
-#col2.metric("Current Price", f'${company.info['currentPrice']}', f"${(company.info['currentPrice'] - company.basic_info['lastPrice']):.3f}")
-  col2.metric("Current Price", f"${infos.get('currentPrice', '')}", get_trend(company))
+col2.metric("Current Price", f"${infos.get('currentPrice', '')}")
 
 # Company name # 
 st.title(infos.get('longName', ''))
@@ -41,10 +39,10 @@ st.title(infos.get('longName', ''))
 st.subheader('Performance')
 
 # Tabs #
-one_day, one_week, one_month, one_year, max = st.tabs(['1 Day',' 1 Week',' 1 Month',' 1 Year', 'Max'])
+one_day, one_week, one_month, one_year, max = st.tabs(['5 Days',' 1 Week',' 1 Month',' 1 Year', 'Max'])
 today = datetime.now().date()
 
-def get_past_date(value, units='days'):
+def get_past_date(value, units):
     if units == 'days':
         past_date = today - timedelta(days=value)
     elif units == 'weeks':
@@ -62,11 +60,11 @@ def update_axis_names(fig, x_axis_name, y_axis_name):
     return fig
 
 with one_day:
- ticker = company.history(period='1d', interval='90m', start=get_past_date(1), end=today) 
+ ticker = company.history(period='1d', start=get_past_date(5, 'days'), end=today) 
  if ticker.empty:
     st.markdown("<p class='no-data'>No Data available</p>", unsafe_allow_html=True)
  else: 
-   fig = px.line(ticker, x = ticker.index, y = ticker['Close'], title = '1 Day')
+   fig = px.line(ticker, x = ticker.index, y = ticker['Close'], title = '5 Days')
    update_axis_names(fig, 'Hour', 'Close Price')
    st.plotly_chart(fig, use_container_width = True)
 
@@ -108,58 +106,72 @@ with max:
 
 st.divider()
 
-# Tab #
-custom_period  = st.tabs(['Custom Period'])
-col1, col2, col3 = st.columns((3))
-with col1:
-  start_date = pd.to_datetime(st.date_input("Start Date", date(date.today().year, 1, 1), min_value=ticker.index.min()))
-with col2:
-  end_date = pd.to_datetime(st.date_input("End Date", date.today()))
-with col3:
-  interval = st.selectbox("Interval", ['1 Day', '5 Days', '1 Week', '1 Month', '3 Months'])
-   
-def switch_inter(value):
-  if value == '1 Day':
-      return '1d'
-  elif value == '5 Days':
-      return '5d'
-  elif value == '1 Week':
-      return '1wk'
-  elif value == '1 Month':
-      return '1mo'
-  elif value == '3 Months':
-      return '3mo'
-  else:
-      return '1d'
 
-ticker = company.history(period='max', interval=switch_inter(interval), start=start_date, end=end_date)
-if ticker.empty:
-  st.markdown("<p class='no-data'>No Data available</p>", unsafe_allow_html=True)
-else:
-  fig = px.line(ticker, x = ticker.index, y = ticker['Close'], title = company.info['longName'])
-  update_axis_names(fig, 'Day', 'Close Price')
-  st.plotly_chart(fig, use_container_width = True)
- 
-# Basic Infos Tabs #
-basic_info = company.basic_info
-if basic_info['dayHigh'] is not None:
-  st.divider()
-  day_high, day_low, year_high, year_low= st.tabs(['Day High',' Day Low',' Year High','Year Low'])
-  with day_high:
-    st.markdown(f"<p class='tab-num'>$ {round(basic_info['dayHigh'], 3)}</p>", unsafe_allow_html=True)
-  with day_low:
-    st.markdown(f"<p class='tab-num'>$ {round(basic_info['dayLow'], 3)}</p>", unsafe_allow_html=True)
-  with year_high:
-    st.markdown(f"<p class='tab-num'>$ {round(basic_info['yearHigh'], 3)}</p>", unsafe_allow_html=True)
-  with year_low:
-    st.markdown(f"<p class='tab-num'>$ {round(basic_info['yearLow'], 3)}</p>", unsafe_allow_html=True)
+# Tabs #
+custom_period, day_high, day_low, year_high, year_low = st.tabs(['Custom Period','Day High',' Day Low',' Year High','Year Low'])
+with custom_period:
+  col1, col2, col3 = st.columns((3))
+  with col1:
+    min_date = ticker.index.min().date() if not ticker.empty else date(date.today().year, 1, 1)
+    start_date = pd.to_datetime(st.date_input("Start Date", date(date.today().year, 1, 1), min_value=min_date))
+  with col2:
+    end_date = pd.to_datetime(st.date_input("End Date", date.today()))
+  with col3:
+    interval = st.selectbox("Interval", ['1 Day', '5 Days', '1 Week', '1 Month', '3 Months'])
+    
+  def switch_inter(value):
+      if value == '1 Day':
+          return '1d'
+      elif value == '5 Days':
+          return '5d'
+      elif value == '1 Week':
+          return '1wk'
+      elif value == '1 Month':
+          return '1mo'
+      elif value == '3 Months':
+          return '3mo'
+      else:
+          return '1d'
+
+  ticker = company.history(period='max', interval=switch_inter(interval), start=start_date, end=end_date)
+  if ticker.empty:
+    st.markdown("<p class='no-data'>No Data available</p>", unsafe_allow_html=True)
+  else:
+    fig = px.line(ticker, x = ticker.index, y = ticker['Close'], title = company.info['longName'])
+    update_axis_names(fig, 'Day', 'Close Price')
+    st.plotly_chart(fig, use_container_width = True)
+
+with day_high:
+  if ticker.empty:
+    st.markdown("<p class='no-data'>No Data available</p>", unsafe_allow_html=True)
+  else:
+   st.markdown(f"<p class='tab-num' style='width:40%; text-align:right;'>$ {round(basic_info['dayHigh'], 3)}</p>", unsafe_allow_html=True)
+
+with day_low:
+  if ticker.empty:
+    st.markdown("<p class='no-data'>No Data available</p>", unsafe_allow_html=True)
+  else:
+    st.markdown(f"<p class='tab-num' style='width:60%; text-align:right;'>$ {round(basic_info['dayLow'], 3)}</p>", unsafe_allow_html=True)
+
+with year_high:
+  if ticker.empty:
+    st.markdown("<p class='no-data'>No Data available</p>", unsafe_allow_html=True)
+  else:
+    st.markdown(f"<p class='tab-num' style='width:80%; text-align:right;'>$ {round(basic_info['yearHigh'], 3)}</p>", unsafe_allow_html=True)
+
+with year_low:
+  if ticker.empty:
+    st.markdown("<p class='no-data'>No Data available</p>", unsafe_allow_html=True)
+  else:
+    st.markdown(f"<p class='tab-num' style='text-align: right;'>$ {round(basic_info['yearLow'], 3)}</p>", unsafe_allow_html=True)
+
 
 # Divider #
 st.divider()
 
 # Display company basic information
 st.subheader("Company Information")
-st.markdown(f"**Company Name:** {company.info['longName']}")
+st.markdown(f"**Company Name:** { infos.get('longName', 'not available')}")
 if infos.get('sector') is not None:
   st.markdown(f"**Sector:** {infos.get('sector', '')}")
 if infos.get('website') is not None:
@@ -181,35 +193,39 @@ st.divider()
 
 # Key Metrics # 
 st.subheader("Key Metrics")
-col4, col5 = st.columns((2))
-with col4:
-   if infos.get('marketCap') is not None:
-     st.metric(label="Market Cap", value=f"$ {infos.get('marketCap', ''):,}")
-   if infos.get('fiftyTwoWeekHigh') is not None:
-    st.metric(label="52 Week High", value=f"$ {infos.get('fiftyTwoWeekHigh', ''):,}")
-   if infos.get('fiftyTwoWeekLow') is not None:
-     st.metric(label="52 Week Low", value=f"$ {infos.get('fiftyTwoWeekLow', ''):,}")
-with col5: 
-  if infos.get('totalRevenue') is not None:
-   st.metric(label="Revenue", value=f"${ infos.get('totalRevenue', ''):,}")
-  if infos.get('ebitda') is not None:
-    st.metric(label="EBITDA", value=f"${ infos.get('ebitda', ''):,}")
-  if infos.get('netIncomeToCommon') is not None:
-   st.metric(label="Net Income", value=f"$ {infos.get('netIncomeToCommon', ''):,}")
+if ticker.empty:
+  st.markdown("<p class='no-data'>No Data available</p>", unsafe_allow_html=True)
+else:
+  col4, col5 = st.columns((2))
+  with col4:
+    if infos.get('marketCap') is not None:
+      st.metric(label="Market Cap", value=f"$ {infos.get('marketCap', ''):,}")
+    if infos.get('fiftyTwoWeekHigh') is not None:
+      st.metric(label="52 Week High", value=f"$ {infos.get('fiftyTwoWeekHigh', ''):,}")
+    if infos.get('fiftyTwoWeekLow') is not None:
+      st.metric(label="52 Week Low", value=f"$ {infos.get('fiftyTwoWeekLow', ''):,}")
+    if financials.loc['Total Revenue'] is not None and financials.loc['Gross Profit'] is not None: 
+      revenue = financials.loc['Total Revenue']
+      gross_profit = financials.loc['Gross Profit']
+      gross_margin = gross_profit / revenue
+      st.metric(label="Gross Margin 2023", value=f"$ {gross_margin.iloc[0]:.2%}")
+  with col5: 
+    if infos.get('totalRevenue') is not None:
+     st.metric(label="Total Revenue", value=f"${ infos.get('totalRevenue', ''):,}")
+    if infos.get('ebitda') is not None:
+      st.metric(label="EBITDA", value=f"${ infos.get('ebitda', ''):,}")
+    if infos.get('netIncomeToCommon') is not None:
+     st.metric(label="Net Income", value=f"$ {infos.get('netIncomeToCommon', ''):,}")
+    if cashflow.loc['Free Cash Flow'][0] is not None:
+     free_cashflow = cashflow.loc['Free Cash Flow'][0]
+     st.metric(label="Free Cash Flow", value=f"$ {int(free_cashflow):,}")
 
-# Actions # 
-actions = company.actions
-if not actions.empty:
-  st.markdown("<p style='margin-bottom: 7px;font-size:14px; color:rgb(49, 51, 6);'>Acitons</p>", unsafe_allow_html=True)
-  #format table..
-  actions.index = actions.index.date
-  actions['Dividends'] = actions['Dividends'].apply(lambda x: f"$ {x:.2f}")
-  actions['Stock Splits'] = actions['Stock Splits'].astype(int)
-  st.write(actions)
-
-#basic_info
-
-#st.write('financials')
-#company.financials
-
-#st.write(infos)
+  # Actions # 
+  actions = company.actions
+  if not actions.empty:
+    st.markdown("<p style='margin-bottom: 7px;font-size:14px; color:rgb(49, 51, 6);'>Acitons</p>", unsafe_allow_html=True)
+    #format table..
+    actions.index = actions.index.date
+    actions['Dividends'] = actions['Dividends'].apply(lambda x: f"$ {x:.2f}")
+    actions['Stock Splits'] = actions['Stock Splits'].astype(int)
+    st.write(actions)
